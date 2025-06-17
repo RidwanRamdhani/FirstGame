0c9d4b974c02b33cdc128dead1217f30f287bf74
Ridwan Ramdhani <gtiridwan@gmail.com>
2025-06-18 00:58:54 +0700

    Score UI

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -4,5 +4,5 @@ extends Area2D
 @onready var pickup_animation: AnimationPlayer = $PickupAnimation
 
 func _on_body_entered(body: Node2D) -> void:
-	game_manager.add_score()
+	game_manager.add_score_coin()
 	pickup_animation.play("pickup")
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,9 +2,13 @@ extends Node
 
 
 var score = 0
+var coin_count = 0
 @onready var score_label: Label = $ScoreLabel
+@onready var score_ui_label: Label = $"../CanvasLayer/ScoreUI/ScoreUILabel"
 
-func add_score():
-	score += 1
-	score_label.text = "You Collected " + str(score) + " Coins."
-	print(score)
+
+func add_score_coin():
+	coin_count += 1
+	score += 10
+	score_label.text = "You Collected " + str(coin_count) + " Coins."
+	score_ui_label.text = "Score: " + str(score)
